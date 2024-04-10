8ccddfb6313682251d5807d9ddd3550208006765
Yan Cesario <yan.cesario@sptech.school>
2024-04-10 20:09:42 -0300

    Adicionando pasta dos cruds de agendamento FORA DA PASTA PADRÃO

---
+++ spring-boot/crud-agendamentos/src/main/kotlin/Kronos/crudagendamentos/CrudAgendamentosApplication.kt
@@ -0,0 +1,11 @@
+package Kronos.crudagendamentos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudAgendamentosApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudAgendamentosApplication>(*args)
+}
---
+++ spring-boot/crud-agendamentos/src/test/kotlin/Kronos/crudagendamentos/CrudAgendamentosApplicationTests.kt
@@ -0,0 +1,13 @@
+package Kronos.crudagendamentos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrudAgendamentosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
