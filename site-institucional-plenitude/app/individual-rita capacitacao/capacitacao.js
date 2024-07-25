208d988ae8c96640b10dfca637fd0ea4764d309e
Rita-de-CassiaB <Rita.Barbosa@sptech.school>
2024-07-24 21:23:06 -0300

    update(): Atualizando CSS perfil - rita

--- site-institucional-plenitude/app/individual-rita capacitacao/capacitacao.js
+++ site-institucional-plenitude/app/individual-rita capacitacao/capacitacao.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const url = 'http://localhost:8080/api/agendamentos/listar';
-    const itemsPerPage = 5; // Número de itens por página
+    const url = 'http://localhost:8080/app/capacitacao/listar';
+    const itemsPerPage = 3; // Número de itens por página
     let currentPage = 1; // Página atual
-    let agendamentos = []; // Array para armazenar os agendamentos
+    let capacitacoes = []; // Array para armazenar as capacitações
 
-    async function fetchAgendamentos() {
+    async function fetchCapacitacoes() {
         try {
             const response = await fetch(url);
             if (!response.ok) {
-                throw new Error('Erro ao carregar os agendamentos.');
+                throw new Error('Erro ao carregar as capacitações.');
             }
             const data = await response.json();
-            agendamentos = data; // Atualiza o array de agendamentos
-            renderTable(); // Renderiza a tabela com os agendamentos atuais
+            capacitacoes = data; // Atualiza o array de capacitações
+            renderTable(); // Renderiza a tabela com as capacitações atuais
         } catch (error) {
-            console.error('Erro ao buscar os agendamentos:', error);
+            console.error('Erro ao buscar as capacitações:', error);
         }
     }
 
@@ -22,120 +22,82 @@ document.addEventListener("DOMContentLoaded", function () {
         const tbody = document.getElementById("procedures-tbody");
         tbody.innerHTML = ''; // Limpa qualquer conteúdo existente
 
-        let totalAgendamentos = agendamentos.length;
-        let confirmados = 0;
+        let totalCapacitacoes = capacitacoes.length;
+        let confirmados = capacitacoes.filter(capacitacao => capacitacao.ativo === 1).length;
 
         // Calcula o índice inicial e final dos itens a serem exibidos na página atual
         const startIndex = (currentPage - 1) * itemsPerPage;
         const endIndex = startIndex + itemsPerPage;
 
-        agendamentos.slice(startIndex, endIndex).forEach(agendamento => {
-            if (agendamento.statusAgendamento.nome === "Confirmado") {
-                confirmados++;
-            }
-
+        capacitacoes.slice(startIndex, endIndex).forEach(capacitacao => {
             const tr = document.createElement("tr");
 
-            // Data/Hora
-            const dataHoraTd = document.createElement("td");
-            dataHoraTd.textContent = `${new Date(agendamento.data).toLocaleDateString()} ${new Date(agendamento.horario).toLocaleTimeString()}`;
-            tr.appendChild(dataHoraTd);
+            // Nome
+            const nomeTd = document.createElement("td");
+            nomeTd.textContent = capacitacao.nome;
+            tr.appendChild(nomeTd);
 
-            // Cliente
-            const clienteTd = document.createElement("td");
-            clienteTd.textContent = agendamento.usuario.nome;
-            tr.appendChild(clienteTd);
+            // Descrição
+            const descricaoTd = document.createElement("td");
+            descricaoTd.textContent = capacitacao.descricao;
+            tr.appendChild(descricaoTd);
 
-            // Procedimento
-            const procedimentoTd = document.createElement("td");
-            procedimentoTd.textContent = agendamento.procedimento.tipo;
-            tr.appendChild(procedimentoTd);
+            // Nível
+            const nivelTd = document.createElement("td");
+            nivelTd.textContent = capacitacao.nivel;
+            tr.appendChild(nivelTd);
 
-            // Especificação
-            const especificacaoTd = document.createElement("td");
-            especificacaoTd.textContent = agendamento.procedimento.descricao;
-            tr.appendChild(especificacaoTd);
+            // Modalidade
+            const modalidadeTd = document.createElement("td");
+            modalidadeTd.textContent = capacitacao.modalidade;
+            tr.appendChild(modalidadeTd);
+
+            // Carga Horária
+            const cargaHorariaTd = document.createElement("td");
+            cargaHorariaTd.textContent = capacitacao.cargaHoraria;
+            tr.appendChild(cargaHorariaTd);
+
+            // Preço
+            const precoTd = document.createElement("td");
+            precoTd.textContent = capacitacao.precoCapacitacao;
+            tr.appendChild(precoTd);
 
             // Status
             const statusTd = document.createElement("td");
-            statusTd.textContent = agendamento.statusAgendamento.nome;
+            statusTd.textContent = capacitacao.ativo ? 'Ativo' : 'Inativo';
             tr.appendChild(statusTd);
-
-            // Ações
-            const acoesTd = document.createElement("td");
-            const editButton = document.createElement("button");
-            editButton.classList.add("edit-btn");
-            editButton.dataset.id = agendamento.idAgendamento;
-            editButton.innerHTML = '<i class="fas fa-edit"></i>';
-            editButton.addEventListener("click", () => editarAgendamento(agendamento.idAgendamento));
-
-            const deleteButton = document.createElement("button");
-            deleteButton.classList.add("delete-btn");
-            deleteButton.dataset.id = agendamento.idAgendamento;
-            deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
-            deleteButton.addEventListener("click", () => excluirAgendamento(agendamento.idAgendamento));
-
-            acoesTd.appendChild(editButton);
-            acoesTd.appendChild(deleteButton);
-            tr.appendChild(acoesTd);
 
             tbody.appendChild(tr);
         });
 
-        // Atualiza a barra de progresso
-        atualizarProgressBar(confirmados, totalAgendamentos);
-
-        // Atualiza a paginação
-        const totalPages = Math.ceil(totalAgendamentos / itemsPerPage);
+        // Atualiza os dados de paginação
         document.getElementById("current-page").textContent = currentPage;
-        document.getElementById("total-pages").textContent = totalPages;
-
-        // Habilita ou desabilita os botões de próxima e página anterior conforme necessário
-        const prevPageBtn = document.getElementById("prev-page-btn");
-        const nextPageBtn = document.getElementById("next-page-btn");
-
-        prevPageBtn.disabled = currentPage === 1;
-        nextPageBtn.disabled = currentPage === totalPages;
+        document.getElementById("total-pages").textContent = Math.ceil(totalCapacitacoes / itemsPerPage);
+        document.getElementById("progress").style.width = `${(confirmados / totalCapacitacoes) * 100}%`;
     }
 
-    function atualizarProgressBar(confirmados, total) {
-        const progress = document.getElementById("progress");
-        const percentage = total === 0 ? 0 : (confirmados / total) * 100;
-        progress.style.width = `${percentage}%`;
-
-        // Atualiza os rótulos com os números
-        document.getElementById("progress-label").textContent = `Atendimentos Confirmados: ${confirmados}`;
-        document.getElementById("total-label").textContent = `Atendimentos Totais: ${total}`;
-    }
-
-    // Eventos para navegação entre páginas
-    document.getElementById("prev-page-btn").addEventListener("click", () => {
+    // Função para ir para a página anterior
+    document.getElementById("prev-page-btn").addEventListener("click", function () {
         if (currentPage > 1) {
             currentPage--;
             renderTable();
         }
+        updatePaginationButtons();
     });
 
-    document.getElementById("next-page-btn").addEventListener("click", () => {
-        const totalPages = Math.ceil(agendamentos.length / itemsPerPage);
-        if (currentPage < totalPages) {
+    // Função para ir para a próxima página
+    document.getElementById("next-page-btn").addEventListener("click", function () {
+        if (currentPage < Math.ceil(capacitacoes.length / itemsPerPage)) {
             currentPage++;
             renderTable();
         }
+        updatePaginationButtons();
     });
 
-    // Inicializa a página
-    fetchAgendamentos();
-});
-
-// Funções para edição e exclusão
-function editarAgendamento(id) {
-    window.location.href = `agendamento-forms/editar-agendamento/editar-agendamento.html?id=${id}`;
-}
-
-function excluirAgendamento(id) {
-    if (confirm("Deseja realmente excluir o agendamento?")) {
-        // Implementar a lógica de exclusão do agendamento aqui
-        alert("Agendamento excluído com sucesso!");
+    function updatePaginationButtons() {
+        document.getElementById("prev-page-btn").disabled = currentPage === 1;
+        document.getElementById("next-page-btn").disabled = currentPage === Math.ceil(capacitacoes.length / itemsPerPage);
     }
-}
+
+    fetchCapacitacoes(); // Chama a função para buscar e renderizar as capacitações
+});
--- site-institucional-plenitude/app/perfil/perfil.js
+++ site-institucional-plenitude/app/perfil/perfil.js
@@ -1,3 +1,120 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const url = 'http://localhost:8080/api/agendamentos/listar';
+    const itemsPerPage = 5; // Número de itens por página
+    let currentPage = 1; // Página atual
+    let agendamentos = []; // Array para armazenar os agendamentos
+
+    let totalAgendamentos = 0;
+    let confirmados = 0;
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+        // Função para carregar os dados do usuário
+        async function carregarDadosUsuario() {
+            try {
+                const response = await fetch('/usuarios/listar-usuarios-ativos'); // Substitua pelo endpoint correto
+                if (response.ok) {
+                    const data = await response.json();
+                    // Assumindo que o retorno é uma lista de usuários
+                    const user = data[0]; // Pegar o primeiro usuário, ajuste conforme necessário
+                    document.getElementById('userName').textContent = user.nome;
+                    document.getElementById('userEmail').textContent = user.email;
+                    document.getElementById('nome').value = user.nome;
+                    document.getElementById('nascimento').value = user.dataNascimento;
+                    document.getElementById('email').value = user.email;
+                    document.getElementById('telefone').value = user.telefone;
+                } else {
+                    console.error('Erro ao carregar dados do usuário:', response.status);
+                }
+            } catch (error) {
+                console.error('Erro ao carregar dados do usuário:', error);
+            }
+        }
+    
+        // Função para atualizar dados do usuário
+        async function atualizarDadosUsuario() {
+            const id = 1; // ID do usuário, ajuste conforme necessário
+            const userData = {
+                nome: document.getElementById('nome').value,
+                dataNascimento: document.getElementById('nascimento').value,
+                email: document.getElementById('email').value,
+                telefone: document.getElementById('telefone').value
+            };
+    
+            try {
+                const response = await fetch(`/usuarios/atualizacao-usuario/${id}`, {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(userData)
+                });
+    
+                if (response.ok) {
+                    const updatedUser = await response.json();
+                    mostrarNotificacao('Dados atualizados com sucesso!');
+                } else {
+                    console.error('Erro ao atualizar dados do usuário:', response.status);
+                }
+            } catch (error) {
+                console.error('Erro ao atualizar dados do usuário:', error);
+            }
+        }
+    
+        // Função para mostrar a notificação
+        function mostrarNotificacao(mensagem) {
+            const notification = document.getElementById('notification');
+            const notificationMessage = document.getElementById('notification-message');
+            notificationMessage.textContent = mensagem;
+            notification.classList.add('show');
+    
+            setTimeout(() => {
+                notification.classList.remove('show');
+            }, 3000);
+        }
+    
+        // Carregar dados do usuário ao carregar a página
+        carregarDadosUsuario();
+    
+        // Adicionar evento ao botão de salvar
+        document.getElementById('save-procedimento-button').addEventListener('click', atualizarDadosUsuario);
+    });
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+// Funções para edição e exclusão
+function editarAgendamento(id) {
+    window.location.href = `agendamento-forms/editar-agendamento/editar-agendamento.html?id=${id}`;
+}
+
+function excluirAgendamento(id) {
+    if (confirm("Deseja realmente excluir o agendamento?")) {
+        // Implementar a lógica de exclusão do agendamento aqui
+        alert("Agendamento excluído com sucesso!");
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const nome = localStorage.getItem('nome');
     const email = localStorage.getItem('email');
