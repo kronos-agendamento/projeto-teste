30dfac8fc6931a102a10d18dd51d77bb68aaef46
ruancrdz <ruan.montanari@sptech.school>
2024-09-08 18:58:58 -0300

    add() pagina cliente html

--- site-institucional-plenitude/app/cliente/cliente.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const nome = localStorage.getItem('nome');
-    const email = localStorage.getItem('email');
-
-    if (nome && email) {
-        document.getElementById('userName').textContent = nome;
-        document.getElementById('userEmail').textContent = email;
-    }
-});
