fc5459afec6b4e5384d8ff4eefc573aac7117867
Gabriel <gabriel.soliveira@sptech.school>
2024-10-03 16:41:22 -0300

    Add() Responsividade página de anamnese mobile

--- site-institucional-plenitude/mobile/ficha-anamnese-mobile/ficha-anamnese-mobile.js
+++ site-institucional-plenitude/mobile/ficha-anamnese-mobile/ficha-anamnese-mobile.js
@@ -6,22 +6,25 @@ document.addEventListener('DOMContentLoaded', function() {
     // Definir tamanho de fonte padrão ou carregar do localStorage
     let currentFontSize = localStorage.getItem('fontSize') || '16px';
     rootElement.style.setProperty('--font-size-default', currentFontSize);
+    document.body.style.fontSize = currentFontSize; // Aplicar o tamanho de fonte ao body
 
     // Função para aumentar o tamanho da fonte
     increaseFontBtn.addEventListener('click', function() {
-        let newSize = parseFloat(currentFontSize) + 1;
+        let newSize = parseFloat(currentFontSize) + 1; // Aumentar 1px
         currentFontSize = `${newSize}px`;
         rootElement.style.setProperty('--font-size-default', currentFontSize);
-        localStorage.setItem('fontSize', currentFontSize);
+        document.body.style.fontSize = currentFontSize; // Aplicar o novo tamanho ao body
+        localStorage.setItem('fontSize', currentFontSize); // Salvar no localStorage
     });
 
     // Função para diminuir o tamanho da fonte
     decreaseFontBtn.addEventListener('click', function() {
-        let newSize = parseFloat(currentFontSize) - 1;
+        let newSize = parseFloat(currentFontSize) - 1; // Diminuir 1px
         if (newSize >= 12) {  // Limitar tamanho mínimo da fonte
             currentFontSize = `${newSize}px`;
             rootElement.style.setProperty('--font-size-default', currentFontSize);
-            localStorage.setItem('fontSize', currentFontSize);
+            document.body.style.fontSize = currentFontSize; // Aplicar o novo tamanho ao body
+            localStorage.setItem('fontSize', currentFontSize); // Salvar no localStorage
         }
     });
 });
